91366f222f62f9ae255e3420ca62b1cb6e4f01ad
Uy Binh <uybinh@gmail.com>
2018-07-29 15:28:58 +0700

    Add basic view, actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,14 +1,27 @@
 class KittensController < ApplicationController
+  before_action :set_kitten, only: [:show, :edit, :destroy]
+
   def index
+    @kittens = Kitten.all
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = "Kitten adopted!"
+      redirect_to root_path
+    else
+      flash.now[:info] = "Something went wrong"
+      render :new
+    end
   end
 
   def show
+
   end
 
   def edit
@@ -18,5 +31,17 @@ class KittensController < ApplicationController
   end
 
   def destroy
+    @kitten.destroy
+    flash[:info] = "The kitten is given away!"
+    redirect_to root_path
   end
+
+  private
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+
+    def set_kitten
+      @kitten = Kitten.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'kittens/index'
-  get 'kittens/new'
-  get 'kittens/show'
-  get 'kittens/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "kittens#index"
+  resources :kittens
 end
